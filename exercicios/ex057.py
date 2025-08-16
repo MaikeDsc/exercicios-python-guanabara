66276810417f09bcbb3793a7b8c92d56181f177a
MaikeDsc <maquiavelcampos@gmail.com>
2025-08-16 17:00:15 -0400

    Atualizando Ex057.py

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -1,1 +1,12 @@
-#escrever
+nome = str(input('informe o seu nome: '))
+sexo= str(input('informe o sexo [M/F]: ').upper)
+
+loop = 1
+while loop == 1:
+    if sexo == "M" or sexo == "F":
+        loop = 0
+        
+    else:
+        sexo = str(input('informe o sexo condizente [M/F]: ').upper())
+
+print(f'Nome: {nome} \nSexo: {sexo}')

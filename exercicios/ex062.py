47a5f12dc1b45759af3ab13220e2b1cbc9deaf10
MaikeDsc <maquiavelcampos@gmail.com>
2025-08-17 13:28:01 -0400

    criação ex062.py

---
+++ exercicios/ex062.py
@@ -0,0 +1,29 @@
+a1 = int(input('primeiro termo: '))
+r = int(input('razão: '))
+
+an = a1
+cont = 0
+
+while cont < 10:
+    cont += 1
+    print(an, end=' ')
+    an += r
+    
+
+loop = True
+while loop == True:
+    termamais = int(input('\n pretende ver mais quantos termos? '))
+    limitloop = cont + termamais
+
+    if termamais > 0:
+        while cont < limitloop:
+            cont += 1
+            print(an, end=' ')
+            an += r
+            
+    elif termamais == 0:
+        loop = False
+    
+print('')
+print(f'fim'.center(25,'='))
+print('')

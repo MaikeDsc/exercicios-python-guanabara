d90696ec34e497c4b5e8ea5500bd2ba271c11b77
MaikeDsc <maquiavelcampos@gmail.com>
2025-08-19 17:21:24 -0400

    fatorial comm exibição de operação

--- exercicios/ex060.py
+++ exercicios/ex060.py
@@ -4,24 +4,26 @@ numa = num
 numb = num #será usado para fazer o mesmo problema, mas usando o for 
 
 #--------------Usando While---------------
-print(f'{numa}! = ', end = '')
+print(f'{numa}! =', end = '')
 fatorial = 1
 numa = numa + 1
 
 while numa > 1:
     numa -= 1
     fatorial  *= numa
-    print(f'{numa} * ', end = '')
+    print(f' {numa} ', end = '')
+    print('*' if numa > 1 else '= ', end='')
 
-print(f'= {fatorial}')
+print(f'{fatorial}')
 
 #-------------Usando for---------------------
 
-print(f'{numb}! = ', end='')
+print(f'{numb}! =', end='')
 
 fatorb = 1
 for c in range (numb, 0, -1):
     fatorb *= c
-    print(f'{c} * ', end='')
+    print(f' {c} ', end='')
+    print('*' if c > 1 else '= ', end='' )
 
-print(f'= {fatorb}')
+print(f'{fatorb}')

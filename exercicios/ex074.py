dcbfd7e58ad7d7901376e0d9343f257c4d61583c
MaikeDsc <maquiavelcampos@gmail.com>
2025-08-27 15:40:56 -0400

    tuplas maior menor

---
+++ exercicios/ex074.py
@@ -0,0 +1,8 @@
+from random import randint
+
+numeros = randint(1,10),randint(1,10),randint(1,10),randint(1,10)
+
+print('sujeridos = ', numeros)
+
+print('maior = ', max(numeros))
+print('menor = ', min(numeros))

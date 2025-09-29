b7c3ada500d9cc98918b02134eed2f4d51f279bb
MaikeDsc <maquiavelcampos@gmail.com>
2025-09-28 21:39:30 -0400

    lista composta, pares e impares - usando o map no print

---
+++ exercicios/ex085.py
@@ -0,0 +1,13 @@
+numeros  = [[],[]]
+
+for c in range(8):
+    
+    n = int(input(f'informe o {c+1}º número:'))
+
+    if n % 2 == 0:
+        numeros[0].append(n)
+    else:
+        numeros[1].append(n)
+
+print('Pares:', ', '.join( map(str, sorted(numeros[0])) ) ) 
+print('Inpares:', ', '.join( map(str, sorted(numeros[1])) ) ) 

84f50ec333712134c0f5d6ca89b3e478c2e95340
MaikeDsc <maquiavelcampos@gmail.com>
2025-09-30 00:33:40 -0400

    cadastro de nota e boletim

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -1,7 +1,8 @@
 dados = []
 
-print('cadastro de notas de alunos')
+print('---------cadastro de notas de alunos-----------')
 while True:
+    print('-=' * 25)
 
     aluno = []
     aluno.append( str(input('Nome aluno: ')))
@@ -11,23 +12,46 @@ while True:
     aluno.append((na+nb)/2)
 
     dados.append(aluno)
+    print('-=' * 25)
 
     op = str(input('Deseja continuar? [s/n]'))
     if op in 'Nn':
         break
 
 
-print('-----------BOLETIM----------')
+print(f'BOLETIM'.center(50, '/'))
 
 for aluno in dados:
     print(f'Nome: {aluno[0]} -> Nota: {aluno[3]}', end= ' ')
     print('(Aprovado)' if aluno[3] >=6 else '(Reprovado)')
-    print('-'*30)
+
+print('/'*57)
 
 pesquisar = str(input('pesquisar aluno [S/N]: '))
 
-'''if pesquisar in 'Nn':
-    while pesquisar in 'Nn':
-'''
+
+while pesquisar in 'Ss':
+
+
+    nome = str(input('nome: ').lower())
+
+    for c in range(len(dados)):
+        
+        if nome in dados[c][0]:
+            print(f'nota de {dados[c][0]} são: {dados[c][1]} e {dados[c][2]}')
+            nao_encontrado= False
+            break
+        
+        else: 
+            nao_encontrado = True
+    
+    if nao_encontrado == True:
+        print('nome não encontrado!')
+
+    print('-=' * 25)
+    pesquisar = str(input('pesquisar aluno [S/N]: '))
+
+else: 
+    print('-------------FIM-------------')
     
 
